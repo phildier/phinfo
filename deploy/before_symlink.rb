861bd470372e7a5fee7de3f819bfcea30b8727eb
Phil Dier <phil@dier.us>
2019-11-21 22:01:45 -0600

    update dump

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,9 +1,13 @@
 log "layer name #{layer_name()}"
-log "new_resource #{new_resource}"
+puts "new_resource #{new_resource}"
 
 bash "run composer install" do
 	cwd release_path
 	code <<-EOH
 	composer install --no-dev --no-ansi --no-interaction
 	EOH
+end
+
+twincities_domains "philtest" do
+	aliases ["philtest.info","philtest.io"]
 end
