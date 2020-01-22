b7f46744390ed8b547b9c6f9ac821ecc28e4e97f
Phil Dier <phil@dier.us>
2020-01-22 08:47:48 -0600

    test new cron resource

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -7,3 +7,9 @@ bash "run composer install" do
 	composer install --no-dev --no-ansi --no-interaction
 	EOH
 end
+
+opsworks_util_cron "test" do
+	servers ["imagex-dev1"]
+	command "touch /tmp/cron-running"
+	minute "*"
+end
